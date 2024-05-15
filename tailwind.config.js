3affdbfc209d36953768bc298e72493359fc740c
gustavoadec <gustavoadec1@gmail.com>
2024-05-15 20:19:08 -0300

    alteração de fonte roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans': ['Roboto','sans-serif']
+
+    },
     extend: {
 /* extendendo uma nova propiedade do tipo background*/
       backgroundImage:{
