9e84082ac48e2c49d7b1f900d347b5a5e1c19dff
gustavoadec <gustavoadec1@gmail.com>
2024-05-17 13:21:33 -0300

    update17/05

---
+++ script.js
@@ -0,0 +1,89 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+// lista de itens
+let cart = [];
+
+//abrir modal carrinho
+cartBtn.addEventListener("click",function () {
+    cartModal.style.display="flex"
+})
+// fechar modal carrinho ao clicar fora
+cartModal.addEventListener("click",function(event){
+if(event.target === cartModal){
+    cartModal.style.display = "none"
+    }
+})
+// botao fechar modal
+closeModalBtn.addEventListener("click", function () {
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+         addToCart(name, price)
+    }
+    
+})
+
+//função para adicionar ao carinho 
+function addToCart(name, price) {
+    alert("o item é" +name)
+  /*  const existingItem = cart.find(item=> item.name === name)
+
+    if (existingItem) {
+        // se o item ja existir,aumenta apenas a quantidade
+        existingItem.quantity +- 1;
+        
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity:1,
+        })
+    }
+    updateCartModal()
+    
+}
+
+//atualiza carinho
+function updateCartModal() {
+    cartItemContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item =>{
+        const cartItemElement = document.createElement("div");
+        
+        cartItemElement.innerHTML =`
+        <div>
+            <div>
+                <p>${item.name}</p>
+                <p>${item.quantity}</p>
+                <p>${item.price}</p>
+            </div>
+            <div>
+                <button>
+                remover
+                </button>
+            </div>
+        </div>
+         `
+         cartItemContainer.appendChild(cartItemElement)
+    })
+
+
+    
+*/}
